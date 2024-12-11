98119e9bcc7ca0fd8145da5b1fb5a98fd014f06d
gabriellreiscantor <68348511+gabriellreiscantor@users.noreply.github.com>
2024-12-11 06:39:02 -0400

    FormPage.tsx

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -8,20 +8,20 @@ const FormPage = () => {
         justifyContent: 'center',
         alignItems: 'center',
         minHeight: '100vh',
-        backgroundColor: '#111827', // Cor de fundo
+        backgroundColor: '#111827', // Fundo escuro
         padding: '20px',
       }}
     >
       <iframe
         data-tally-src="https://tally.so/r/mVo5o6?transparentBackground=1"
         style={{
-          border: 'none', // Remove a borda
+          border: 'none', // Remove bordas do iframe
           width: '100%',
-          maxWidth: '600px', // Limita a largura para centralizar
-          height: 700, // Define a altura como número
+          maxWidth: '600px',
+          height: 700, // Altura ajustada
         }}
-        marginHeight={0} // Corrigido para tipo number
-        marginWidth={0} // Corrigido para tipo number
+        marginHeight={0} // Ajustado para o tipo number
+        marginWidth={0} // Ajustado para o tipo number
         title="Formulário Tally"
       ></iframe>
     </div>
