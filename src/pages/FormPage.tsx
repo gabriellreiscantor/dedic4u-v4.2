c2f5b5130f5b1603590fc2614a342c356f74f00e
gabriellreiscantor <68348511+gabriellreiscantor@users.noreply.github.com>
2024-12-11 06:54:39 -0400

    FormPage.tsx

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -4,8 +4,10 @@ const FormPage = () => {
       <iframe
         src="https://tally.so/embed/mVo5o6?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
         title="Formulário"
+        marginHeight={0}
+        marginWidth={0}
         frameBorder="0"
-        style={{ width: "100%", height: "100vh" }}
+        style={{ width: "100%", height: "100vh", border: "none" }}
       ></iframe>
     </div>
   );
