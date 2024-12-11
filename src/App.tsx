52cbf5470f887d666d45178082a48af0ed10c4a5
gabriellreiscantor <68348511+gabriellreiscantor@users.noreply.github.com>
2024-12-11 05:37:22 -0400

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { PersonalizedPage } from './pages/PersonalizedPage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { PaymentPage } from './pages/PaymentPage';
 import { SuccessPage } from './pages/SuccessPage';
+import FormPage from './pages/FormPage'; // Página do Formulário
+import ThankYouPage from './pages/ThankYouPage'; // Página de Agradecimento
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/success" element={<SuccessPage />} />
+        <Route path="/form" element={<FormPage />} /> {/* Rota do Formulário */}
+        <Route path="/thank-you" element={<ThankYouPage />} /> {/* Rota de Agradecimento */}
         <Route path="/:slug" element={<PersonalizedPage />} />
       </Routes>
     </div>
