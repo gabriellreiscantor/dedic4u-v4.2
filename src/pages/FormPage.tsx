d78c6ce572679ff400cff20483c060799d433d94
gabriellreiscantor <68348511+gabriellreiscantor@users.noreply.github.com>
2024-12-11 07:01:32 -0400

    FormPage.tsx

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,13 +1,15 @@
-const FormPage = () => {
+import React from "react";
+
+const FormPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Formulário</h1>
       <iframe
         src="https://tally.so/embed/mVo5o6?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-        title="Formulário"
-        marginHeight={0}
-        marginWidth={0}
+        width="100%"
+        height="1000"
         frameBorder="0"
-        style={{ width: "100%", height: "100vh", border: "none" }}
+        title="Formulário"
       ></iframe>
     </div>
   );
