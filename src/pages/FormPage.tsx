0a6d7926e31581ee07f94bc932c796b131535796
gabriellreiscantor <68348511+gabriellreiscantor@users.noreply.github.com>
2024-12-11 07:08:13 -0400

    FormPage.tsx

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -3,14 +3,15 @@ import React from "react";
 const FormPage: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Formulário</h1>
       <iframe
         src="https://tally.so/embed/mVo5o6?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
         width="100%"
-        height="1000"
+        height={1000}
         frameBorder="0"
+        marginHeight={0} // Valores corrigidos
+        marginWidth={0}  // Valores corrigidos
         title="Formulário"
-      ></iframe>
+      />
     </div>
   );
 };
